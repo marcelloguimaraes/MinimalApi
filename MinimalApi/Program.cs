37550f73bfddc5b7462c423bfac6a85c9d73aaa9
Marcello Guimaraes <marcello.gpc@hotmail.com>
2021-10-09 14:49:35 -0300

    finish endpoints

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,6 +1,40 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using MinimalApi.Data;
+using MinimalApi.ViewModels;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+ConfigureDbContext(builder.Services);
+ConfigureSwagger(builder.Services);
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("v1/todos", (AppDbContext context) => {
+    var todos = context.Todos.ToList();
+    return Results.Ok(todos);
+});
+
+app.MapPost("v1/todos", (AppDbContext context, CreateTodoViewModel viewModel) => {
+    var todo = viewModel.ValidateAndMapTo();
+    if (!viewModel.IsValid)
+    {
+        return Results.BadRequest(viewModel.Notifications);
+    }
+
+    context.Todos.Add(todo);
+    context.SaveChanges();
+    return Results.Created($"v1/todos/{todo.Id}", todo);
+});
+
+void ConfigureSwagger(IServiceCollection services)
+{
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+}
+
+void ConfigureDbContext(IServiceCollection services) 
+{
+    services.AddDbContext<AppDbContext>();
+}
 
 app.Run();
